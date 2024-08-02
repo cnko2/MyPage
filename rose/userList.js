8bea933d857d0d5baa7dbf8732a53e0da42e8970
cnko2 <61863099+cnko2@users.noreply.github.com>
2024-08-02 09:17:35 +0800

    Update userList.js

--- rose/userList.js
+++ rose/userList.js
@@ -1,7 +1,8 @@
 // userList.js
 export const userList = [
     { username: "1071388", chineseName: "柯朝南" },
-    { username: "user2", chineseName: "李四" },
-    { username: "user3", chineseName: "王五" }
+    { username: "1090581", chineseName: "杜御爾" },
+    { username: "1000420", chineseName: "楊智維" }
+    { username: "991170", chineseName: "楊明杰" }
     // Add more users as needed
 ];
