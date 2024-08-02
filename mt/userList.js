c6d413bb0659ca8d4c12c28f8778585d4acedb46
cnko2 <61863099+cnko2@users.noreply.github.com>
2024-08-02 10:46:10 +0800

    Create userList.js

---
+++ mt/userList.js
@@ -0,0 +1,5 @@
+// userList.js
+export const userList = [
+    { username: "991364", chineseName: "陳基文" , url: "https://crm-api.ocard.co/thirdParty/auth/kyeckingyuan?s=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiI5OTEzNjQifQ.GB0noZt-F_8oefOjxAuUY_l6UDiChxRdRgZy3JxiUVY"},
+    // Add more users as needed
+];
