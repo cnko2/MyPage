02cd1c02c48539f59cb2013ad24c662b38ab6347
cnko2 <61863099+cnko2@users.noreply.github.com>
2024-08-02 09:31:10 +0800

    Update userList.js

--- rose/userList.js
+++ rose/userList.js
@@ -1,8 +1,7 @@
 // userList.js
 export const userList = [
-    { username: "1071388", chineseName: "柯朝南" , url: ""},
-    { username: "1090581", chineseName: "杜御爾" , url: ""},
-    { username: "1000420", chineseName: "楊智維" , url: ""},
-    { username: "991170", chineseName: "楊明杰" , url: ""}
+    { username: "1090581", chineseName: "杜御爾" , url: "https://crm-api.ocard.co/thirdParty/auth/kyeckingyuan?s=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiIxMDkwNTgxIn0.JcNfmBRhX_LvL528aep972BVutMrKJ9XZxvT-NWywtQ"},
+    { username: "1000420", chineseName: "楊智維" , url: "https://crm-api.ocard.co/thirdParty/auth/kyeckingyuan?s=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiIxMDAwNDIwIn0.Jt-Pkde-mgtPAT2sgk7E3Fv1ZD42V2EnJ84hsQs51Ps"},
+    { username: "991170", chineseName: "楊明杰" , url: "https://crm-api.ocard.co/thirdParty/auth/kyeckingyuan?s=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJpYWwiOiI5OTExNzAifQ.UlTxkfXY_50O6RmJmb0F1MradyQqRoW33oi1xMnOvPk"}
     // Add more users as needed
 ];
